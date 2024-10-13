9a009924f918da467fcfb406d3582843db3d95e2
alipala.ist@gmail.com <alipala.ist@gmail.com>
2024-10-13 14:07:12 +0200

    Uncomment render app

--- app.py
+++ app.py
@@ -464,7 +464,7 @@ def handle_get_pgn():
     pgn = export_game_to_pgn(board)
     emit('pgn_data', {'pgn': pgn})
 
+
 if __name__ == '__main__':
     logger.info("Starting the Chess AI application")
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, debug=False, host='0.0.0.0', port=5001)
